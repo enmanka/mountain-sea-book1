982d61ee51fb27a4a615dee038ed37678305dd21
yuanyuan6065 <1842755140@qq.com>
2024-05-16 08:16:46 +0800

    12

--- mountain-sea-book1/src/components/page4.vue
+++ mountain-sea-book1/src/components/page4.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="img1">
         <el-container>
-          <el-aside width="400px">
+          <el-aside width="300px">
               <el-header><index4/></el-header>
               <el-main>
               </el-main>
